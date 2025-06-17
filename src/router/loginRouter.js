d7676ecaed53338be1010c64f1a69e883e3c3647
Anshad M <anshadabdulla9111@gmail.com>
2025-06-17 14:05:26 +0530

    fix

--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const LoginController = require('../Controllers/loginController');
 const loginController = new LoginController();
+const auth = require('../middleware/auth');
 
 router.post('/login', loginController.login);
-router.post('/forgotPassword', loginController.forgotPassword);
-router.post('/resetPassword', loginController.resetPassword);
+router.post('/forgotPassword',auth, loginController.forgotPassword);
+router.post('/resetPassword',auth, loginController.resetPassword);
 
 
 module.exports = router;
